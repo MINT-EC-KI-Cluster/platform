7634d6131213a80d37f9aba906fa0ad1aee87491
Tim Arnold <46609883+Coder246@users.noreply.github.com>
2024-02-12 18:29:25 +0100

    init commit

---
+++ theme.config.jsx
@@ -0,0 +1,44 @@
+export default {
+    logo: <span>Lerne mit KI-Projekten und starte eigene!</span>,
+    project: {
+      link: 'https://github.com/MINT-EC-KI-Cluster',
+
+    },
+    docsRepositoryBase: 'https://github.com/MINT-EC-KI-Cluster/platform',
+
+    search: {
+        placeholder: 'Suche',
+    },
+    banner: {
+        key: 'a',
+        text: (
+          <a href="https://github.com/MINT-EC-KI-Cluster" target="_blank">
+            Aktuell in Arbeit. Github Repo →
+          </a>
+        )
+      },
+    useNextSeoProps() {
+        return {
+          titleTemplate: '%s – MINT-EC KI-Cluster',
+        }
+      },
+
+      footer: {
+        text: (
+          <span>
+            © {new Date().getFullYear()} MINT-EC KI-Cluster
+          </span>
+        )
+      },
+
+      themeSwitch: {
+        useOptions() {
+          return {
+            light: 'Hell',
+            dark: 'Dunkel',
+            system: 'System'
+          }
+        }
+      }
+    // ... other theme options
+  }
